1e7c215bb9bea0269270d3ec1599fd38185fa4b3
voronovskii <voronovyyy@yandex.ru>
2024-02-27 10:47:07 +0300

    add test check

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,6 +6,7 @@ import java.util.Map;
 
 public class Differ {
 
+    private static final String DEFAULT = "stylish";
 
     public static String generate(String firstFilePath, String secondFilePath, String format) throws Exception {
         String content1 = FileUtils.readFile(firstFilePath);
@@ -16,5 +17,9 @@ public class Differ {
         Map<String, Object> data2 = Parser.getData(contain2, extension2);
         Map<String, Item> differ = Differences.getDiff(data1, data2);
         return Formatter.getFormat(differ, format);
+    }
+
+    public static String generate(String filePath1, String filePath2) throws Exception {
+        return generate(filePath1, filePath2, "stylish");
     }
 }
